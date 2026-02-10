f69b9482f042605c706c981487da1984d19b9e7f
Nitin singh <rush2nitin1@gmail.com>
2026-02-10 23:24:02 +0530

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -8,18 +8,13 @@ public class InsertionSort {
        }
        System.out.println();
    }
-
-
-   public static void main(String args[]) {
+  public static void main(String args[]) {
        int arr[] = {7, 8, 1, 3, 2};
 
-
-       //insertion sort
        for(int i=1; i<arr.length; i++) {
            int current = arr[i];
            int j = i - 1;
                while(j >= 0 && arr[j] > current) {
-                   //Keep swapping
                    arr[j+1] = arr[j];
                    j--;
                }
